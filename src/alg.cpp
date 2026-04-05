66cd358b58b5514531391e916b123aa073b88588
weddiiii <150699840+weddiiii@users.noreply.github.com>
2026-04-05 23:01:00 +0300

    Refactor countPairs functions and variable names
    Refactor variable names for clarity and improve formatting.

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,8 @@
 // Copyright 2026 NNTU-CS
-
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        
         if (i > 0 && arr[i] == arr[i - 1]) continue;
-
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
@@ -18,7 +15,7 @@ int countPairs1(int *arr, int len, int value) {
 
 int binarySearch(int *arr, int left, int right, int target) {
     while (left <= right) {
-        int mid = left + (right - left) / 2; // чуть другая запись
+        int mid = left + (right - left) / 2;
         if (arr[mid] == target) return 1;
         else if (arr[mid] < target) left = mid + 1;
         else right = mid - 1;
@@ -26,33 +23,31 @@ int binarySearch(int *arr, int left, int right, int target) {
     return 0;
 }
 
+
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
         if (i > 0 && arr[i] == arr[i - 1]) continue;
-
         int target = value - arr[i];
-        if (target < arr[i]) continue;  // отсечение лишних
-
+        if (target < arr[i]) continue; 
         if (binarySearch(arr, i + 1, len - 1, target)) count++;
     }
     return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    int l = 0, r = len - 1;
+    int left = 0, right = len - 1;
 
-    while (l < r) {
-        int sum = arr[l] + arr[r];
-
+    while (left < right) {
+        int sum = arr[left] + arr[right];
         if (sum == value) {
             count++;
-            int lv = arr[l], rv = arr[r];
-            while (l < len && arr[l] == lv) l++;
-            while (r >= 0 && arr[r] == rv) r--;
-        } 
-        else if (sum < value) l++;
-        else r--;
+            int lv = arr[left], rv = arr[right];
+            while (left < len && arr[left] == lv) left++;
+            while (right >= 0 && arr[right] == rv) right--;
+        } else if (sum < value) left++;
+        else right--;
     }
     return count;
 }
